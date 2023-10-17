3f98d8963da2625697177d57d0cd45c54524672a
roshan-bh91 <roshan@betterhalf.ai>
2023-10-17 15:20:42 +0530

    [chore]: project 1 setup with vite, ts & tailwind

---
+++ project-1/src/App.tsx
@@ -0,0 +1,16 @@
+const App = () => {
+  return (
+    <main>
+      Nav
+      <section className="xl:padding-1 wide:padding-r padding-b">Hero</section>
+      <section className="padding">Popular Products</section>
+      <section className="padding">Super Quality</section>
+      <section className="padding-x py-10">Services</section>
+      <section className="padding">Special offers</section>
+      <section className="bg-pale-blue padding">Customer reviews</section>
+      <section className="padding-x sm:py-32 py-16">Subscribe</section>
+      <section className="padding-x padding-t pb-8 bg-black">Footer</section>
+    </main>
+  );
+};
+export default App;
