ee618e60d307415e2b83f7c3f70ed8b7643b3bc0
roshan-bh91 <roshan@betterhalf.ai>
2023-10-18 18:27:23 +0530

    [style]: right section of the hero added with dynamic shoe card change

--- project-1/src/components/ShoeCard.tsx
+++ project-1/src/components/ShoeCard.tsx
@@ -1,7 +1,41 @@
-interface Props {}
+interface Props {
+  imgDetails: {
+    thumbnail: string;
+    bigShoe: string;
+  };
+  changeBigShoeImg: (bigShoeToSet: {
+    thumbnail: string;
+    bigShoe: string;
+  }) => void;
+  bigShoeImg: string;
+}
 
-const ShoeCard = () => {
-  return <div>ShoeCard</div>;
+const ShoeCard = ({ imgDetails, bigShoeImg, changeBigShoeImg }: Props) => {
+  const handleClick = () => {
+    if (bigShoeImg !== imgDetails.bigShoe) {
+      changeBigShoeImg(imgDetails);
+    }
+  };
+  return (
+    <div
+      className={`border-2 rounded-xl ${
+        bigShoeImg === imgDetails.bigShoe
+          ? "border-coral-red"
+          : "border-transparent"
+      } cursor-pointer max-sm:flex-1`}
+      onClick={handleClick}
+    >
+      <div className="flex justify-center items-center bg-cover bg-center bg-card sm:w-40 sm:h-40 max-sm:p-4">
+        <img
+          src={imgDetails.thumbnail}
+          alt="Shoe collection"
+          width={127}
+          height={103}
+          className="object-contain"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default ShoeCard;
--- project-1/src/sections/Hero.tsx
+++ project-1/src/sections/Hero.tsx
@@ -1,8 +1,17 @@
+import { useState } from "react";
 import { arrowRight } from "../assets/icons";
-import { Button } from "../components";
-import { statistics } from "../constants";
+import { bigShoe1 } from "../assets/images";
+import { Button, ShoeCard } from "../components";
+import { shoes, statistics } from "../constants";
 
 const Hero = () => {
+  const [bigShoeImg, setBigShoeImg] = useState(bigShoe1);
+  const handleShoeCardClick = (shoeDetails: {
+    thumbnail: string;
+    bigShoe: string;
+  }) => {
+    setBigShoeImg(shoeDetails.bigShoe);
+  };
   return (
     <section
       id="home"
@@ -34,6 +43,26 @@ const Hero = () => {
               <p className="leading-7 font-montserrat text-slate-gray">
                 {label}
               </p>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="flex items-center justify-center relative flex-1 xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
+        <img
+          src={bigShoeImg}
+          alt="Big shoe Image"
+          className="object-contain relative z-10"
+          width={610}
+          height={502}
+        />
+        <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6">
+          {shoes.map((everyShoe) => (
+            <div key={everyShoe.bigShoe}>
+              <ShoeCard
+                imgDetails={everyShoe}
+                changeBigShoeImg={handleShoeCardClick}
+                bigShoeImg={bigShoeImg}
+              />
             </div>
           ))}
         </div>
