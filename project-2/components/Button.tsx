02e1014878dcb8829efc47038802fa80b0a9bf41
roshan-bh91 <roshan@betterhalf.ai>
2023-11-14 19:59:40 +0530

    [ui]: call to action for download section

--- project-2/components/Button.tsx
+++ project-2/components/Button.tsx
@@ -6,20 +6,26 @@ interface ButtonProps {
   title: string;
   icon?: string;
   variant?: ComponentProps<"button">["className"];
+  full?: boolean;
 }
 const Button = ({
   type = "button",
   title,
   icon,
   variant = "",
+  full = false,
 }: ButtonProps) => {
   return (
     <button
-      className={`flexCenter gap-3 rounded-full border ${variant}`}
+      className={`flexCenter gap-3 rounded-full border ${variant} ${
+        full && `w-full`
+      }`}
       type={type}
     >
       {icon && <Image alt={title} width={24} height={24} src={icon} />}
-      <label className="bold-16 whitespace-nowrap">{title}</label>
+      <label className="bold-16 whitespace-nowrap cursor-pointer">
+        {title}
+      </label>
     </button>
   );
 };
--- project-2/components/GetApp.tsx
+++ project-2/components/GetApp.tsx
@@ -1,5 +1,46 @@
+import Image from "next/image";
+import { Button } from ".";
+
 const GetApp = () => {
-  return <div>GetApp</div>;
+  return (
+    <section className="flexCenter flex-col w-full pb-[100px]">
+      <div className="get-app">
+        <div className="flex flex-1 flex-col justify-center items-start gap-12 w-full z-20">
+          <h2 className="bold-40 lg:bold-64 xl:max-w-[320px]">
+            Get for free now!
+          </h2>
+          <p className="regular-16 text-gray-10">
+            Available on iOS and Android
+          </p>
+          <div className="w-full flex flex-col xl:flex-row gap-3 whitespace-nowrap">
+            <Button
+              type="button"
+              title={"App Store"}
+              icon="/apple.svg"
+              variant="btn_white"
+              full
+            />
+
+            <Button
+              type="button"
+              title="Play Store"
+              icon="/android.svg"
+              variant="btn_dark_green_outline"
+              full
+            />
+          </div>
+        </div>
+        <div className="flex flex-1 items-center justify-end">
+          <Image
+            src={"/phones.png"}
+            alt="Multiple phones"
+            height={870}
+            width={550}
+          />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default GetApp;
